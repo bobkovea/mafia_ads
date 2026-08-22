91ffaaf9f8ff0902646d8362b452e2af17b2dea0
bobkovea <52193810+bobkovea@users.noreply.github.com>
2026-08-22 14:04:36 +0500

    godfather completed

--- melodies/godfather.h
+++ melodies/godfather.h
@@ -21,13 +21,23 @@ enum class Duration : uint16_t {
 #define N(note, dur) { NOTE_##note, (uint16_t)Duration::dur }
 
 const Note melody[] PROGMEM = {
-  N(B3, Q), N(E4, Q), N(G4, Q),
+  N(REST, Q), N(B3, Q), N(E4, Q), N(G4, Q),
   N(FS4, Q), N(E4, Q), N(G4, Q), N(E4, Q), N(FS4, Q), N(E4, Q), N(C4, Q), N(D4, Q),
   N(B3, W),
   
-  N(B3, Q), N(E4, Q), N(G4, Q),
+  N(REST, Q), N(B3, Q), N(E4, Q), N(G4, Q),
   N(FS4, Q), N(E4, Q), N(G4, Q), N(E4, Q), N(FS4, Q), N(E4, Q), N(B3, Q), N(AS3, Q),
   N(A3, W),
+  
+  N(REST, Q), N(A3, Q), N(C4, Q), N(DS4, Q),
+  N(FS4, W),
+  N(REST, Q), N(A3, Q), N(C4, Q), N(DS4, Q),
+  N(E4, W),
+  
+  N(REST, Q), N(E3, Q), N(G3, Q), N(D4, Q),
+  N(C4, Q), N(B3, Q), N(D4, Q), N(C4, Q),
+  N(C4, Q), N(B3, Q), N(B3, Q), N(DS4, Q),
+  N(E4, W)
 };
 
 const uint16_t melodyLength = sizeof(melody) / sizeof(melody[0]);
