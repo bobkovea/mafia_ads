422b07259de05d6e44dc8b86905f5642a10f9b53
bobkovea <52193810+bobkovea@users.noreply.github.com>
2026-08-25 20:34:03 +0500

    pirates up to 2,5 tones

--- melodies/pirates.h
+++ melodies/pirates.h
@@ -12,59 +12,59 @@ static constexpr uint16_t Tempo = 1050U;
 
 const Note melody[] PROGMEM =
 {
-/*
-  N(E4, E), N(G4, E), N(A4, Q), N(A4, E), N(REST, E),
-  N(A4, E), N(B4, E), N(C5, Q), N(C5, E), N(REST, E),
-  N(C5, E), N(D5, E), N(B4, Q), N(B4, E), N(REST, E),
-  N(A4, E), N(G4, E), N(A4, DQ), N(REST, E),
 
-  N(E4, E), N(G4, E), N(A4, Q), N(A4, E), N(REST, E),
-  N(A4, E), N(B4, E), N(C5, Q), N(C5, E), N(REST, E),
-  N(C5, E), N(D5, E), N(B4, Q), N(B4, E), N(REST, E),
-  N(A4, E), N(G4, E), N(A4, DQ), N(REST, E),
+	/*N(A4, E), N(C5, E), N(D5, Q), N(D5, E), N(REST, E),
+	N(D5, E), N(E5, E), N(F5, Q), N(F5, E), N(REST, E),
+	N(F5, E), N(G5, E), N(E5, Q), N(E5, E), N(REST, E),
+	N(D5, E), N(C5, E), N(D5, DQ), N(REST, E),
 
-  N(E4, E), N(G4, E), N(A4, Q), N(A4, E), N(REST, E),
-  N(A4, E), N(C5, E), N(D5, Q), N(D5, E), N(REST, E),
-  N(D5, E), N(E5, E), N(F5, Q), N(F5, E), N(REST, E),
-  N(E5, E), N(D5, E), N(E5, E), N(A4, Q), N(REST, E),
+	N(A4, E), N(C5, E), N(D5, Q), N(D5, E), N(REST, E),
+	N(D5, E), N(E5, E), N(F5, Q), N(F5, E), N(REST, E),
+	N(F5, E), N(G5, E), N(E5, Q), N(E5, E), N(REST, E),
+	N(D5, E), N(C5, E), N(D5, DQ), N(REST, E),
 
-  N(A4, E), N(B4, E), N(C5, Q), N(C5, E), N(REST, E),
-  N(D5, Q), N(E5, E), N(A4, Q), N(REST, E),
-  N(A4, E), N(C5, E), N(B4, Q), N(B4, E), N(REST, E),
-  N(C5, E), N(A4, E), N(B4, DQ), N(REST, DQ),
+	N(A4, E), N(C5, E), N(D5, Q), N(D5, E), N(REST, E),
+	N(D5, E), N(F5, E), N(G5, Q), N(G5, E), N(REST, E),
+	N(G5, E), N(A5, E), N(AS5, Q), N(AS5, E), N(REST, E),
+	N(A5, E), N(G5, E), N(A5, E), N(D5, Q), N(REST, E),
 
-  N(A4, Q), N(A4, E),*/
+	N(D5, E), N(E5, E), N(F5, Q), N(F5, E), N(REST, E),
+	N(G5, Q), N(A5, E), N(D5, Q), N(REST, E),
+	N(D5, E), N(F5, E), N(E5, Q), N(E5, E), N(REST, E),
+	N(F5, E), N(D5, E), N(E5, DQ), N(REST, DQ),
+
+  N(D5, Q), N(D5, E),*/
   //Repeat of first part
-  N(E4, E), N(G4, E), N(A4, Q), N(A4, E), N(REST, E),
-  N(A4, E), N(B4, E), N(C5, Q), N(C5, E), N(REST, E),
-  N(C5, E), N(D5, E), N(B4, Q), N(B4, E), N(REST, E),
-  N(A4, E), N(G4, E), N(A4, DQ), N(REST, E),
+	N(A4, E), N(C5, E), N(D5, Q), N(D5, E), N(REST, E),
+	N(D5, E), N(E5, E), N(F5, Q), N(F5, E), N(REST, E),
+	N(F5, E), N(G5, E), N(E5, Q), N(E5, E), N(REST, E),
+	N(D5, E), N(C5, E), N(D5, DQ), N(REST, E),
 
-  N(E4, E), N(G4, E), N(A4, Q), N(A4, E), N(REST, E),
-  N(A4, E), N(B4, E), N(C5, Q), N(C5, E), N(REST, E),
-  N(C5, E), N(D5, E), N(B4, Q), N(B4, E), N(REST, E),
-  N(A4, E), N(G4, E), N(A4, DQ), N(REST, E),
+	N(A4, E), N(C5, E), N(D5, Q), N(D5, E), N(REST, E),
+	N(D5, E), N(E5, E), N(F5, Q), N(F5, E), N(REST, E),
+	N(F5, E), N(G5, E), N(E5, Q), N(E5, E), N(REST, E),
+	N(D5, E), N(C5, E), N(D5, DQ), N(REST, E),
 
-  N(E4, E), N(G4, E), N(A4, Q), N(A4, E), N(REST, E),
-  N(A4, E), N(C5, E), N(D5, Q), N(D5, E), N(REST, E),
-  N(D5, E), N(E5, E), N(F5, Q), N(F5, E), N(REST, E),
-  N(E5, E), N(D5, E), N(E5, E), N(A4, Q), N(REST, E),
+	N(A4, E), N(C5, E), N(D5, Q), N(D5, E), N(REST, E),
+	N(D5, E), N(F5, E), N(G5, Q), N(G5, E), N(REST, E),
+	N(G5, E), N(A5, E), N(AS5, Q), N(AS5, E), N(REST, E),
+	N(A5, E), N(G5, E), N(A5, E), N(D5, Q), N(REST, E),
 
-  N(A4, E), N(B4, E), N(C5, Q), N(C5, E), N(REST, E),
-  N(D5, Q), N(E5, E), N(A4, Q), N(REST, E),
-  N(A4, E), N(C5, E), N(B4, Q), N(B4, E), N(REST, E),
-  N(C5, E), N(A4, E), N(B4, DQ), N(REST, DQ),
-  //End of Repeat
+	N(D5, E), N(E5, E), N(F5, Q), N(F5, E), N(REST, E),
+	N(G5, Q), N(A5, E), N(D5, Q), N(REST, E),
+	N(D5, E), N(F5, E), N(E5, Q), N(E5, E), N(REST, E),
+	N(F5, E), N(D5, E), N(E5, DQ), N(REST, DQ),
+	//End of Repeat
 
-  N(E5, Q), N(REST, E), N(REST, DQ), N(F5, Q), N(REST, E), N(REST, DQ),
-  N(E5, E), N(E5, E), N(REST, E), N(G5, E), N(REST, E), N(E5, E), N(D5, E), N(REST, E), N(REST, DQ),
-  N(D5, Q), N(REST, E), N(REST, DQ), N(C5, Q), N(REST, E), N(REST, DQ),
-  N(B4, E), N(C5, E), N(REST, E), N(B4, E), N(REST, E), N(A4, H),
+	N(A5, Q), N(REST, E), N(REST, DQ), N(AS5, Q), N(REST, E), N(REST, DQ),
+	N(A5, E), N(A5, E), N(REST, E), N(C6, E), N(REST, E), N(A5, E), N(G5, E), N(REST, E), N(REST, DQ),
+	N(G5, Q), N(REST, E), N(REST, DQ), N(F5, Q), N(REST, E), N(REST, DQ),
+	N(E5, E), N(F5, E), N(REST, E), N(E5, E), N(REST, E), N(D5, H),
 
-  N(E5, Q), N(REST, E), N(REST, DQ), N(F5, Q), N(REST, E), N(REST, DQ),
-  N(E5, E), N(E5, E), N(REST, E), N(G5, E), N(REST, E), N(E5, E), N(D5, E), N(REST, E), N(REST, DQ),
-  N(D5, Q), N(REST, E), N(REST, DQ), N(C5, Q), N(REST, E), N(REST, DQ),
-  N(B4, E), N(C5, E), N(REST, E), N(B4, E), N(REST, E), N(A4, H),
+	N(A5, Q), N(REST, E), N(REST, DQ), N(AS5, Q), N(REST, E), N(REST, DQ),
+	N(A5, E), N(A5, E), N(REST, E), N(C6, E), N(REST, E), N(A5, E), N(G5, E), N(REST, E), N(REST, DQ),
+	N(G5, Q), N(REST, E), N(REST, DQ), N(F5, Q), N(REST, E), N(REST, DQ),
+	N(E5, E), N(F5, E), N(REST, E), N(E5, E), N(REST, E), N(D5, H)
 };
 
 const uint16_t melodyLength = sizeof(melody) / sizeof(melody[0]);
