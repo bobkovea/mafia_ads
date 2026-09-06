398b5d9cc06572b63d9bc26ae10f96d5d853d3aa
bobkovea <52193810+bobkovea@users.noreply.github.com>
2026-09-07 00:18:19 +0500

    fix

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static constexpr uint8_t BuzzerPin = 3;
 static constexpr uint8_t BacklightPin = 9;
 static constexpr uint8_t LcdI2cAddress = 0x27;
 static constexpr uint8_t LcdRowsCount = 2;
-static constexpr uint8_t LcdColsCount = 0x27;
+static constexpr uint8_t LcdColsCount = 16;
 
 static const char* loadingMessages[] =
 {
